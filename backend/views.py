d671caabbc63f7728be281c11ecc472f12818323
unknown <1600057886@qq.com>
2019-04-09 17:14:48 +0800

    frontend

--- backend/views.py
+++ backend/views.py
@@ -4,9 +4,13 @@ from django.shortcuts import render
 
 from django.shortcuts import HttpResponse
 from backend import models
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+
 # 导入models模块
 def orm(request):
-# 创建数据
+    # 创建数据
     # 第一种方式
     models.UserInfo.objects.create(username="root", password="123")
     # 第二种方式
@@ -14,11 +18,16 @@ def orm(request):
     # obj.save()
     # result = models.UserInfo.objects.all()  # 查询所有，为QuerySet类型，可理解成列表
 
-# 删除数据
-#     models.UserInfo.objects.all().delete()  # 删除所有
+    # 删除数据
+    #     models.UserInfo.objects.all().delete()  # 删除所有
 
-# 更新数据
+    # 更新数据
     # models.UserInfo.objects.all().update(password=8888)
     # models.UserInfo.objects.filter(id=3).update(password=888888)
 
     return HttpResponse('orm')
+
+
+@csrf_exempt
+def test_api(request):
+    return JsonResponse({"result": 0, "msg": "执行成功"})
--- kzyd/urls.py
+++ kzyd/urls.py
@@ -18,9 +18,12 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import url
 from backend import views
+from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^orm/', views.orm),
+    url(r'^test_api/', views.test_api),
+    url(r'', TemplateView.as_view(template_name="index.html")),
 ]
